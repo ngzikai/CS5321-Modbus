29f8b36badda40218d3b3909d2408c9a69fb6e42
Mustaqiim Muhar <mustaqiim.muhar@u.nus.edu>
2018-05-10 15:10:00 +0800

    working multithread, broken dual communication

--- src/HmiThread.java
+++ src/HmiThread.java
@@ -16,18 +16,27 @@ class HmiServer extends Thread{
 	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
 	private Thread t;
 	private String threadName;
+	private byte[] sharedSecret;
 
-	HmiServer(String name) {
+	HmiServer(String name, byte[] secret) {
 		threadName = name;
+		sharedSecret = secret;
 		System.out.println("Creating " + threadName);
 	}
 
 	public void run(){
 		
-		byte[] sharedSecret = generateKey();
-		System.out.println("Shared Secret: " + bytesToHex(sharedSecret));
+		if (threadName.equals("SEND")) {
+			System.out.println("This is send la");
+			sendApc(sharedSecret);
+		} else if (threadName.equals("RECEIVE")){
+			System.out.println("THIS IS RECEIVE YAY");
+			receiveSlave(sharedSecret);
+		}
+		// byte[] sharedSecret = generateKey();
+		// System.out.println("Shared Secret: " + bytesToHex(sharedSecret));
 	
-		sendApc(sharedSecret);
+		
 	}
 
 	public void start() {
@@ -128,6 +137,146 @@ class HmiServer extends Thread{
 	}
 	
 
+	// private static byte[] generateKey() {
+	// 	/*
+	// 	 * Alice creates her own DH key pair with 2048-bit key size
+	// 	 */
+	// 	try {
+	// 		//System.out.println("ALICE: Generate DH keypair ...");
+	// 		KeyPairGenerator aliceKpairGen = KeyPairGenerator.getInstance("DH");
+	// 		aliceKpairGen.initialize(2048);
+	// 		KeyPair aliceKpair = aliceKpairGen.generateKeyPair();
+
+	// 		// Alice creates and initializes her DH KeyAgreement object
+	// 		//System.out.println("ALICE: Initialization ...");
+	// 		KeyAgreement aliceKeyAgree = KeyAgreement.getInstance("DH");
+	// 		aliceKeyAgree.init(aliceKpair.getPrivate());
+
+	// 		// Alice encodes her public key, and sends it over to Bob.
+	// 		byte[] alicePubKeyEnc = aliceKpair.getPublic().getEncoded();
+
+	// 		ServerSocket slaveServer = new ServerSocket(1234);
+
+	// 		Socket connectionSocket = slaveServer.accept();
+
+	// 		if (connectionSocket != null) {
+	// 			System.out.println("Accepted Bob at " + connectionSocket.getInetAddress());
+	// 		}
+
+	// 		DataOutputStream out = new DataOutputStream(connectionSocket.getOutputStream());
+	// 		InputStream inStream = connectionSocket.getInputStream();
+	// 		byte[] dataBuffer = new byte[10000];
+
+	// 		//System.out.println("Sending over Alice's Public Key");
+	// 		out.write(alicePubKeyEnc);
+
+	// 		// Alice Recieves Bob's Public Key
+	// 		//System.out.println("Recieving Bob's Public Key");
+	// 		int count = inStream.read(dataBuffer);
+
+	// 		byte[] bobPubKeyEnc = new byte[count];
+
+	// 		for (int i = 0; i < bobPubKeyEnc.length; i++) {
+	// 			bobPubKeyEnc[i] = dataBuffer[i];
+	// 		}
+
+	// 		/*
+	// 		 * Alice uses Bob's public key for the first (and only) phase of her version of
+	// 		 * the DH protocol. Before she can do so, she has to instantiate a DH public key
+	// 		 * from Bob's encoded key material.
+	// 		 */
+	// 		KeyFactory aliceKeyFac = KeyFactory.getInstance("DH");
+	// 		X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(bobPubKeyEnc);
+	// 		PublicKey bobPubKey = aliceKeyFac.generatePublic(x509KeySpec);
+	// 		aliceKeyAgree.doPhase(bobPubKey, true);
+
+	// 		/*
+	// 		 * At this stage, both Alice and Bob have completed the DH key agreement
+	// 		 * protocol. Both generate the (same) shared secret.
+	// 		 */
+	// 		byte[] aliceSharedSecret = aliceKeyAgree.generateSecret();
+			
+	// 		slaveServer.close();
+	// 		return aliceSharedSecret;
+	// 	} catch (Exception e) {
+	// 		System.out.println("Error Generating Shared Secret");
+	// 		return null;
+	// 	}
+
+	// }
+
+	/*
+	 * Converts a byte to hex digit and writes to the supplied buffer
+	 */
+	private static void byte2hex(byte b, StringBuffer buf) {
+		char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+		int high = ((b & 0xf0) >> 4);
+		int low = (b & 0x0f);
+		buf.append(hexChars[high]);
+		buf.append(hexChars[low]);
+	}
+
+	/*
+	 * Converts a byte array to hex string
+	 */
+	private static String toHexString(byte[] block) {
+		StringBuffer buf = new StringBuffer();
+		int len = block.length;
+		for (int i = 0; i < len; i++) {
+			byte2hex(block[i], buf);
+			if (i < len - 1) {
+				buf.append(":");
+			}
+		}
+		return buf.toString();
+	}
+
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
+
+	public static byte[] hexToBytes(String s) {
+	    int len = s.length();
+	    byte[] data = new byte[len / 2];
+	    for (int i = 0; i < len; i += 2) {
+	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+	                             + Character.digit(s.charAt(i+1), 16));
+	    }
+	    return data;
+	}
+}
+
+public class HmiThread {
+	public static void main (String args[]){
+
+		byte[] sharedSecret = generateKey();
+		System.out.println("Shared Secret: " + bytesToHex(sharedSecret));
+
+		HmiServer send = new HmiServer("SEND", sharedSecret);
+		send.start();
+
+		HmiServer receive = new HmiServer("RECEIVE", sharedSecret);
+		receive.start();
+	}
+
+	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
+
 	private static byte[] generateKey() {
 		/*
 		 * Alice creates her own DH key pair with 2048-bit key size
@@ -194,58 +343,5 @@ class HmiServer extends Thread{
 			return null;
 		}
 
-	}
-
-	/*
-	 * Converts a byte to hex digit and writes to the supplied buffer
-	 */
-	private static void byte2hex(byte b, StringBuffer buf) {
-		char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-		int high = ((b & 0xf0) >> 4);
-		int low = (b & 0x0f);
-		buf.append(hexChars[high]);
-		buf.append(hexChars[low]);
-	}
-
-	/*
-	 * Converts a byte array to hex string
-	 */
-	private static String toHexString(byte[] block) {
-		StringBuffer buf = new StringBuffer();
-		int len = block.length;
-		for (int i = 0; i < len; i++) {
-			byte2hex(block[i], buf);
-			if (i < len - 1) {
-				buf.append(":");
-			}
-		}
-		return buf.toString();
-	}
-
-	public static String bytesToHex(byte[] bytes) {
-	    char[] hexChars = new char[bytes.length * 2];
-	    for ( int j = 0; j < bytes.length; j++ ) {
-	        int v = bytes[j] & 0xFF;
-	        hexChars[j * 2] = hexArray[v >>> 4];
-	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-	    }
-	    return new String(hexChars);
-	}
-
-	public static byte[] hexToBytes(String s) {
-	    int len = s.length();
-	    byte[] data = new byte[len / 2];
-	    for (int i = 0; i < len; i += 2) {
-	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-	                             + Character.digit(s.charAt(i+1), 16));
-	    }
-	    return data;
-	}
-}
-
-public class HmiThread {
-	public static void main (String args[]){
-		HmiServer send = new HmiServer("HMI SEND");
-		send.start();
 	}
 }
